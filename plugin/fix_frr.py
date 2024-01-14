f5b6db86d104814659f85636e14bec2a465fd9eb
Ivan Pepelnjak <ip@ipspace.net>
2024-01-14 10:26:54 +0100

    Bug fix: do not remove FRR config_template binds
    38f1403 removed clab.config_templates parameter on FRR to override
    binding of /etc/frr/daemons. That worked for the user router,
    but crashed the provider router if it also used FRR.
    
    This fix retains the frr.clab.config_templates (so FRR works as X1
    1-basic exercise), but replaces the clab binds for RTR (so the BGP
    daemon is started without having BGP module)

--- plugin/fix_frr.py
+++ plugin/fix_frr.py
@@ -4,9 +4,24 @@ from netsim import __version__
 from netsim.utils import log
 from box import Box
 
-def init(topology: Box) -> None:
-  topology.defaults.devices.frr.clab.node.pop('config_templates')
+"""
+The initial lab exercises do not use BGP configuration module on the customer
+device because we want the user to start BGP configuration from scratch.
 
+That approach does not work for FRR containers because you cannot restart FRR
+after modifying /etc/frr/daemons -- restating FRR kills the top-level container
+process. Cumulus Linux does not have the same problem as it runs a different
+init process.
+
+This plugin is a fix for the FRR container initialization: if the user is using
+clab as primary or secondary provider and the RTR device is a FRR container, the
+clab binds are replaced with a mapping of FRR daemons into a local file. The
+modifier FRR daemons file starts BGP, OSPFv2 and OSPFv3 (so we have a generic
+solution in case we need it somewhere else).
+
+Finally, the plugin adds "we already started BGP daemon for you" message to the
+topology message to tell the user what's going on.
+"""
 def post_node_transform(topology: Box) -> None:
   uses_clab = topology.get('provider',None) == 'clab' or topology.nodes.rtr.get('provider',None) == 'clab'
   uses_frr  = topology.nodes.rtr.device == 'frr'
